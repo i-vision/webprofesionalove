4e86386ae82af551c48ee51c2fcf0eac3675a8c7
George <webcorporate.cz@gmail.com>
2012-01-25 00:47:20 +0100

    Changed encoding of tranjslations files to utf-8

--- qa-lang/cs/qa-lang-emails.php
+++ qa-lang/cs/qa-lang-emails.php
@@ -25,55 +25,55 @@
 */
 
 	return array(
-		'a_commented_body' => "Vaše odpovìï ^site_title má nový komentáø na ^c_handle:\n\n^open^c_content^close\n\nVaše odpovìï byla:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_commented_subject' => 'Vaše ^site_title odpovìï má nový komentáø',
+		'a_commented_body' => "VaÅ¡e odpovÄ›Ä ^site_title mÃ¡ novÃ½ komentÃ¡Å™ na ^c_handle:\n\n^open^c_content^close\n\nVaÅ¡e odpovÄ›Ä byla:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
+		'a_commented_subject' => 'VaÅ¡e ^site_title odpovÄ›Ä mÃ¡ novÃ½ komentÃ¡Å™',
 
-		'a_followed_body' => "Vaše odpovìï na ^site_title má novou navazující otázku  ^q_handle:\n\n^open^q_title^close\n\nVaše odpovìï byla :\n\n^open^a_content^close\n\nKlikni dolu k odpovìzení na novou otázku:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_followed_subject' => 'Vaše ^site_title odpovìï má související otázku.',
+		'a_followed_body' => "VaÅ¡e odpovÄ›Ä na ^site_title mÃ¡ novou navazujÃ­cÃ­ otÃ¡zku  ^q_handle:\n\n^open^q_title^close\n\nVaÅ¡e odpovÄ›Ä byla :\n\n^open^a_content^close\n\nKlikni dolu k odpovÄ›zenÃ­ na novou otÃ¡zku:\n\n^url\n\nThank you,\n\n^site_title",
+		'a_followed_subject' => 'VaÅ¡e ^site_title odpovÄ›Ä mÃ¡ souvisejÃ­cÃ­ otÃ¡zku.',
 
-		'a_selected_body' => "Blahopøejeme! Vaše odpovìï na ^site_title byla vybrána jako nejlepší ^s_handle:\n\n^open^a_content^close\n\nThe question was:\n\n^open^q_title^close\n\nClick below to see your answer:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_selected_subject' => 'Vaše ^site_title odpovìï byla vybrána!',
+		'a_selected_body' => "BlahopÅ™ejeme! VaÅ¡e odpovÄ›Ä na ^site_title byla vybrÃ¡na jako nejlepÅ¡Ã­ ^s_handle:\n\n^open^a_content^close\n\nThe question was:\n\n^open^q_title^close\n\nClick below to see your answer:\n\n^url\n\nThank you,\n\n^site_title",
+		'a_selected_subject' => 'VaÅ¡e ^site_title odpovÄ›Ä byla vybrÃ¡na!',
 
-		'c_commented_body' => "Nový komentáø od ^c_handle byl pøidán po tom, co byl váš komentáø na ^site_title:\n\n^open^c_content^close\n\nThe diskuze je následující:\n\n^open^c_context^close\n\nYou may respond by adding another comment:\n\n^url\n\nThank you,\n\n^site_title",
-		'c_commented_subject' => 'Váš ^site_title komentáø byl pøidán',
+		'c_commented_body' => "NovÃ½ komentÃ¡Å™ od ^c_handle byl pÅ™idÃ¡n po tom, co byl vÃ¡Å¡ komentÃ¡Å™ na ^site_title:\n\n^open^c_content^close\n\nThe diskuze je nÃ¡sledujÃ­cÃ­:\n\n^open^c_context^close\n\nYou may respond by adding another comment:\n\n^url\n\nThank you,\n\n^site_title",
+		'c_commented_subject' => 'VÃ¡Å¡ ^site_title komentÃ¡Å™ byl pÅ™idÃ¡n',
 
-		'confirm_body' => "Prosím kliknìte níže, abyste potvrdili Vaši emailovou adresu pro ^site_title.\n\n^url\n\nThank you,\n^site_title",
-		'confirm_subject' => '^site_title - Potvrzení emailové adresy',
+		'confirm_body' => "ProsÃ­m kliknÄ›te nÃ­Å¾e, abyste potvrdili VaÅ¡i emailovou adresu pro ^site_title.\n\n^url\n\nThank you,\n^site_title",
+		'confirm_subject' => '^site_title - PotvrzenÃ­ emailovÃ© adresy',
 
-		'feedback_body' => "Komentáøe:\n^message\n\nName:\n^name\n\nEmail:\n^email\n\nPrevious page:\n^previous\n\nUser:\n^url\n\nIP address:\n^ip\n\nBrowser:\n^browser",
+		'feedback_body' => "KomentÃ¡Å™e:\n^message\n\nName:\n^name\n\nEmail:\n^email\n\nPrevious page:\n^previous\n\nUser:\n^url\n\nIP address:\n^ip\n\nBrowser:\n^browser",
 		'feedback_subject' => '^ feedback',
 
-		'flagged_body' => "Pøíspìvek od ^p_handle byl pøijat ^flags:\n\n^open^p_context^close\n\nklikni níže ke shlédnutí pøíspìvku:\n\n^url\n\nThank you,\n\n^site_title",
-		'flagged_subject' => '^site_title má ovlajkovaný pøíspìvek',
+		'flagged_body' => "PÅ™Ã­spÄ›vek od ^p_handle byl pÅ™ijat ^flags:\n\n^open^p_context^close\n\nklikni nÃ­Å¾e ke shlÃ©dnutÃ­ pÅ™Ã­spÄ›vku:\n\n^url\n\nThank you,\n\n^site_title",
+		'flagged_subject' => '^site_title mÃ¡ ovlajkovanÃ½ pÅ™Ã­spÄ›vek',
 
-		'moderate_body' => "Pøíspìvek by ^p_handle vyžaduje Vaše schválení:\n\n^open^p_context^close\n\nClick níže ke schválení nebo odmítnutí pøíspìvku:\n\n^url\n\nThank you,\n\n^site_title",
-		'moderate_subject' => '^site_title øízení',
+		'moderate_body' => "PÅ™Ã­spÄ›vek by ^p_handle vyÅ¾aduje VaÅ¡e schvÃ¡lenÃ­:\n\n^open^p_context^close\n\nClick nÃ­Å¾e ke schvÃ¡lenÃ­ nebo odmÃ­tnutÃ­ pÅ™Ã­spÄ›vku:\n\n^url\n\nThank you,\n\n^site_title",
+		'moderate_subject' => '^site_title Å™Ã­zenÃ­',
 
-		'new_password_body' => "Vaše nové heslo pro ^site_title je níže.\n\nPassword: ^password\n\nIt je doporuèován ke zmìnì tohoto hesla okamžitì po pøihlášení .\n\nThank you,\n^site_title\n^url",
-		'new_password_subject' => '^site_title - Vaše nové heslo',
+		'new_password_body' => "VaÅ¡e novÃ© heslo pro ^site_title je nÃ­Å¾e.\n\nPassword: ^password\n\nIt je doporuÄovÃ¡n ke zmÄ›nÄ› tohoto hesla okamÅ¾itÄ› po pÅ™ihlÃ¡Å¡enÃ­ .\n\nThank you,\n^site_title\n^url",
+		'new_password_subject' => '^site_title - VaÅ¡e novÃ© heslo',
 
-		'private_message_body' => "Byla Vám zaslána soukromá zpráva od ^f_handle on ^site_title:\n\n^open^message^close\n\n^moreThank you,\n\n^site_title\n\n\nK blokaci soukromých zpráv, navštivte stránku Vašeho úètu:\n^a_url",
-		'private_message_info' => "Více informací o ^f_handle:\n\n^url\n\n",
-		'private_message_reply' => "Klikni níže k odpovìzení na ^f_handle soukromou zprávu:\n\n^url\n\n",
-		'private_message_subject' => 'Zpráva od ^f_handle na ^site_title',
+		'private_message_body' => "Byla VÃ¡m zaslÃ¡na soukromÃ¡ zprÃ¡va od ^f_handle on ^site_title:\n\n^open^message^close\n\n^moreThank you,\n\n^site_title\n\n\nK blokaci soukromÃ½ch zprÃ¡v, navÅ¡tivte strÃ¡nku VaÅ¡eho ÃºÄtu:\n^a_url",
+		'private_message_info' => "VÃ­ce informacÃ­ o ^f_handle:\n\n^url\n\n",
+		'private_message_reply' => "Klikni nÃ­Å¾e k odpovÄ›zenÃ­ na ^f_handle soukromou zprÃ¡vu:\n\n^url\n\n",
+		'private_message_subject' => 'ZprÃ¡va od ^f_handle na ^site_title',
 
-		'q_answered_body' => "Vaše otázka ^site_title byla zodpovìzena ^a_handle:\n\n^open^a_content^close\n\nVaše otázka byla:\n\n^open^q_title^close\n\nPokud se Vám líbí tato odpovìï, mùžete ji oznaèit jako nejlepší:\n\n^url\n\nThank you,\n\n^site_title",
-		'q_answered_subject' => 'Vaše ^site_title otázka byla zodpovìzena',
+		'q_answered_body' => "VaÅ¡e otÃ¡zka ^site_title byla zodpovÄ›zena ^a_handle:\n\n^open^a_content^close\n\nVaÅ¡e otÃ¡zka byla:\n\n^open^q_title^close\n\nPokud se VÃ¡m lÃ­bÃ­ tato odpovÄ›Ä, mÅ¯Å¾ete ji oznaÄit jako nejlepÅ¡Ã­:\n\n^url\n\nThank you,\n\n^site_title",
+		'q_answered_subject' => 'VaÅ¡e ^site_title otÃ¡zka byla zodpovÄ›zena',
 
-		'q_commented_body' => "Vaše otázka na ^site_title má nový komentáø od ^c_handle:\n\n^open^c_content^close\n\nVaše otázka byla:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
-		'q_commented_subject' => 'Vaše ^site_title otázka má nový komentáø',
+		'q_commented_body' => "VaÅ¡e otÃ¡zka na ^site_title mÃ¡ novÃ½ komentÃ¡Å™ od ^c_handle:\n\n^open^c_content^close\n\nVaÅ¡e otÃ¡zka byla:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
+		'q_commented_subject' => 'VaÅ¡e ^site_title otÃ¡zka mÃ¡ novÃ½ komentÃ¡Å™',
 
-		'q_posted_body' => "Nová otázka byla položena ^q_handle:\n\n^open^q_title\n\n^q_content^close\n\nKlikni níže ke shlédnutí otázky:\n\n^url\n\nThank you,\n\n^site_title",
-		'q_posted_subject' => '^site_title má novou otázku',
+		'q_posted_body' => "NovÃ¡ otÃ¡zka byla poloÅ¾ena ^q_handle:\n\n^open^q_title\n\n^q_content^close\n\nKlikni nÃ­Å¾e ke shlÃ©dnutÃ­ otÃ¡zky:\n\n^url\n\nThank you,\n\n^site_title",
+		'q_posted_subject' => '^site_title mÃ¡ novou otÃ¡zku',
 
-		'reset_body' => "Prosím kliknìte níže k obnovení Vašeho hesla pro ^site_title.\n\n^url\n\nAlternativnì, zadejte kód níže do poskytnutého pole.\n\nCode: ^code\n\nPokud jste nepožádali o obnovení Vašeho hesla, ignorujte prosím tuto zprávu.\n\nThank you,\n^site_title",
-		'reset_subject' => '^site_title - Obnovit zapomenuté heslo',
+		'reset_body' => "ProsÃ­m kliknÄ›te nÃ­Å¾e k obnovenÃ­ VaÅ¡eho hesla pro ^site_title.\n\n^url\n\nAlternativnÄ›, zadejte kÃ³d nÃ­Å¾e do poskytnutÃ©ho pole.\n\nCode: ^code\n\nPokud jste nepoÅ¾Ã¡dali o obnovenÃ­ VaÅ¡eho hesla, ignorujte prosÃ­m tuto zprÃ¡vu.\n\nThank you,\n^site_title",
+		'reset_subject' => '^site_title - Obnovit zapomenutÃ© heslo',
 
 		'to_handle_prefix' => "^,\n\n",
 
-		'welcome_body' => "Dìkujeme Vám, že jste se zaregistrovali na ^site_title.\n\n^custom^confirmYour login details are as follows:\n\nEmail: ^email\nPassword: ^password\n\nProsím udržujte tuto informaci v bezpeèí pro budoucí reference.\n\nThank you,\n\n^site_title\n^url",
-		'welcome_confirm' => "Prosím kliknìte níže k potvrzení Vaši emailové adresy.\n\n^url\n\n",
-		'welcome_subject' => 'Vítejte na ^site_title!',
+		'welcome_body' => "DÄ›kujeme VÃ¡m, Å¾e jste se zaregistrovali na ^site_title.\n\n^custom^confirmYour login details are as follows:\n\nEmail: ^email\nPassword: ^password\n\nProsÃ­m udrÅ¾ujte tuto informaci v bezpeÄÃ­ pro budoucÃ­ reference.\n\nThank you,\n\n^site_title\n^url",
+		'welcome_confirm' => "ProsÃ­m kliknÄ›te nÃ­Å¾e k potvrzenÃ­ VaÅ¡i emailovÃ© adresy.\n\n^url\n\n",
+		'welcome_subject' => 'VÃ­tejte na ^site_title!',
 	);
 	
 
